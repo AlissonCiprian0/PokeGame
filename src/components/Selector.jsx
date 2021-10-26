08ce60a994439a1029b6ef877f1204d88589718e
Alisson Cipriano <58789035+AlissonCiprian0@users.noreply.github.com>
2021-10-26 19:31:11 -0300

    router-fix-2

--- src/components/Selector.jsx
+++ src/components/Selector.jsx
@@ -19,7 +19,7 @@ const Selector = ({baseAPI, pokemons}) => {
     });
 
     const handleButtonClick = () => {
-        if (!location.includes('/battle')) {
+        if (location.includes('/battle')) {
             setHand1(hand1.shift());
             
             let localHand1 = JSON.stringify(hand1);
